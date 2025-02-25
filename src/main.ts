b4c067bc65f802c6f7a8b16f211a4d5bdcca2415
이승원 <lsw4315@jobkorea.co.kr>
2025-02-25 12:32:54 +0900

    Fix cors studio.apollographql.com 허용

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,15 @@ import { GraphQLSchema } from 'graphql';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  // cors 허용
 
+  app.enableCors({
+    origin:  ['https://studio.apollographql.com'], // https://studio.apollographql.com/sandbox/explorer 에서 테스트 가능 (전부혀용 true).
+    credentials: true, // 쿠키, 인증 헤더 등을 사용여부
+    exposedHeaders: ['Authorization'], // * 사용할 헤더 추가.
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
   // GraphQL Schema 가져오기
   const gqlSchemaHost = app.get(GraphQLSchemaHost);
   const schema: GraphQLSchema = gqlSchemaHost.schema;
@@ -14,4 +21,5 @@ async function bootstrap() {
   // 스키마 정보 출력 (예: 콘솔 로그로 확인)
   console.log('Generated GraphQL Schema:', schema);
 }
+
 bootstrap();
