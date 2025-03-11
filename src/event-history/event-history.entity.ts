d626807cf3246d87e5a09dabb356a7d633ca8d8e
roum02 <roum02@ewhain.net>
2025-03-12 01:42:21 +0900

    Add event history table

--- src/event-history/event-history.entity.ts
+++ src/event-history/event-history.entity.ts
@@ -1,30 +1,40 @@
+import { Field, ID, ObjectType } from '@nestjs/graphql';
 import { Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { EventPage } from '../events/events.entity';
+import GraphQLJSON from 'graphql-type-json';
 
+@ObjectType()
 @Entity('event_history')
 export class EventHistory {
+  @Field(() => ID)
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Field(() => EventPage)
   @ManyToOne(() => EventPage, { onDelete: 'CASCADE' })
   @JoinColumn({ name: 'event_page_id' })
   eventPage: EventPage;
 
+  @Field(() => GraphQLJSON)
   @Column({ type: 'jsonb', nullable: false })
   previousPageJson: object;
 
+  @Field(() => GraphQLJSON)
   @Column({ type: 'jsonb', nullable: false })
   changedPageJson: object;
 
+  @Field()
   @Column({ type: 'text', nullable: true })
   changeReason: string; // 변경 사유 (선택 사항)
 
   @Column({ type: 'text', nullable: true })
   testCol: string; // 변경 사유 (선택 사항)
 
+  @Field()
   @Column({ type: 'varchar', length: 100 })
   changedBy: string; // 변경한 사용자
 
+  @Field()
   @CreateDateColumn({ name: 'changed_at' })
   changedAt: Date; // 변경 일자
 }
--- src/event-history/event-history.module.ts
+++ src/event-history/event-history.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventHistory } from './event-history.entity';
+import { EventHistoryService } from './event-history.service';
+import { EventHistoryResolver } from './event-history.resolver';
+import { EventPage } from '../events/events.entity';
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([EventHistory]), // 엔티티 추가
-  ],
+  imports: [TypeOrmModule.forFeature([EventHistory, EventPage])],
+  providers: [EventHistoryService, EventHistoryResolver],
+  exports: [EventHistoryService],
 })
 export class EventHistoryModule {}
